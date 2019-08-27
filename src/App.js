2fa73f5d144ddb9f3c3ed62754f19121f993e15b
mykindofscum <alvinkchan99@gmail.com>
2019-08-26 21:52:10 -0700

    Add VideoList, VideoItem and VideoList functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
 
+import { Grid } from '@material-ui/core';
+
+import { SearchBar, VideoDetail, VideoList } from  './components';
+
+import youtube from './api/youtube';
+
 class App extends React.Component {
+  state = {
+    videos: [],
+    selectedVideo: null
+  }
+
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: 'AIzaSyCylZ5s6ZpIFtQ-9UiLm8b9ixyzJcD2bwk',
+        q: searchTerm,
+      }
+    });
+    
+    this.setState ({ videos: response.data.items, selectedVideo: response.data.items[0] });
+  }  
+
   render (){
+    const { selectedVideo, videos } = this.state; 
     return (
-      <h1>YouTube Clone App</h1>
+      <Grid justify="center" container spacing={10}>
+        <Grid item xs={12}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={this.handleSubmit} />
+            </Grid>
+              <Grid item xs={8}>
+                <VideoDetail video={selectedVideo} />
+              </Grid>
+                <Grid item xs={4}>
+                  <VideoList videos={videos} />
+                </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     )
   }
 }
